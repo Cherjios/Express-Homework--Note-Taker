04cc498042b019c30fe313d657f9d8f2837febbc
cherjios <slopez0286@gmail.com>
2020-07-17 20:03:12 -0700

    adding routes functions

--- server.js
+++ server.js
@@ -13,19 +13,11 @@ app.use(express.json());
 
 //Routes
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
   });
   
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
   });
   
-  app.get("/all", function(req, res) {
-    res.sendFile(path.join(__dirname, "all.html"));
-  });
-  
-  // Displays all characters
-  app.get("/api/characters", function(req, res) {
-    return res.json(characters);
-  });
