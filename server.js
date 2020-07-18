8eebcfdc8c38f998e5e9af549978b487d9d77acc
cherjios <slopez0286@gmail.com>
2020-07-17 19:59:34 -0700

    Adding the express app in server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 // Dependencies
 var express = require("express");
 var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
